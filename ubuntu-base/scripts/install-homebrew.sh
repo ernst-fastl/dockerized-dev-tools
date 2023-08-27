515f0472b3b6032d385b9901c6331374f4a9a093
Ernst Fastl <ernst@fastl.eu>
2023-08-27 07:43:16 +0000

    changed homebrew to global install, added .env to .gitignore

--- ubuntu-base/scripts/install-homebrew.sh
+++ ubuntu-base/scripts/install-homebrew.sh
@@ -1,24 +1,28 @@
 #!/bin/bash
 
-# Check if script is running with root privileges
+# Check if script is running as root
 if [ "$(id -u)" != "0" ]; then
     echo "This script must be run as root"
     exit 1
 fi
 
+# Install curl and git via custom script
 /opt/devbox/apt-clean-install.sh curl git
 
-# Set the path to the install script
+# Set path to the Homebrew installation script
 INSTALL_SCRIPT="/opt/devbox/homebrew-head-install.sh"
 
-# Download the install script
+# Download Homebrew installation script
 curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh -o "$INSTALL_SCRIPT"
 
-# Set the NONINTERACTIVE environment variable
+# Set NONINTERACTIVE environment variable
 export NONINTERACTIVE=1
 
-# Run the install script
+# Run Homebrew installation script
 /bin/bash "$INSTALL_SCRIPT"
 
-# Add the brew shellenv command to /etc/profile
-echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /etc/profile
+# Move Homebrew installation to global path
+mv /home/linuxbrew/.linuxbrew /usr/local/
+
+# Add brew shellenv command to /etc/profile
+echo 'eval "$(/usr/local/bin/brew shellenv)"' >> /etc/profile
