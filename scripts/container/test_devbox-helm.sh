d63b3dcc18d8c59aa6b9431c5a73df997e668888
Ernst Fastl <ernst@fastl.eu>
2023-08-29 18:28:09 +0000

    stage 1 working with tests

--- scripts/container/test_devbox-helm.sh
+++ scripts/container/test_devbox-helm.sh
@@ -4,8 +4,8 @@ source /opt/devbox/test_installed_tool_versions.sh
 
 # Call check_version for each tool with its corresponding version command
 check_version "Docker" "docker --version"
-check_version "Docker Compose" "docker compose --version"
-check_version "Kubectl" "kubectl version --client --short"
+check_version "Docker Compose" "docker compose version"
+check_version "Kubectl" "kubectl version --client"
 check_version "Helm" "helm version --short"
 check_version "Helmfile" "helmfile --version"
 
