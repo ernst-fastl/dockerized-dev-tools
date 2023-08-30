a9ca46c1f41d91c05b20feee72102562098ed763
Ernst Fastl <ernst@fastl.eu>
2023-08-30 10:51:46 +0000

    signal handling in entrypoint

--- devbox-base/entrypoint.sh
+++ devbox-base/entrypoint.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-# endless loop to keep container running
+echo "Container started $(date)"
+echo "Entering endless loop to keep container running while waiting for IDEs to connect..."
+echo "Press [CTRL+C] to stop..."
+
+trap "echo 'Signal caught, exiting!'; exit;" SIGINT SIGTERM
 sleep infinity
+
