21f02f7626538df5cf8fc4efa87a23540236cf2a
Ernst Fastl <ernst@fastl.eu>
2023-09-01 13:23:37 +0000

    added sourcing of /etc/profile to .bashrc since vscode skips that

--- ubuntu-base/custom-profiled-loader.sh
+++ ubuntu-base/custom-profiled-loader.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-# custom-profiled-loader.sh
-
+export CUSTOM_PROFILED_LOADER_RAN=1
 if [ ! -z "${SOURCE_SCRIPTS_FOLDER}" ]; then
   for script in ${SOURCE_SCRIPTS_FOLDER}/*; do
     source "$script"
