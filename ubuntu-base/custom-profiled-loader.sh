e3ada56e4027c41b0dbab11b33cea566195603a9
Ernst Fastl <ernst@fastl.eu>
2023-09-09 13:28:13 +0000

    fixed custom profile d loader not executed in new shells

--- ubuntu-base/custom-profiled-loader.sh
+++ ubuntu-base/custom-profiled-loader.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-export CUSTOM_PROFILED_LOADER_RAN=1
+echo "$(date +"%Y-%m-%d %T") - Running custom profiled loader in ${SOURCE_SCRIPTS_FOLDER}" >> /tmp/custom-profiled-loader.log
 if [ ! -z "${SOURCE_SCRIPTS_FOLDER}" ]; then
   for script in ${SOURCE_SCRIPTS_FOLDER}/*; do
+    echo "$(date +"%Y-%m-%d %T") - Sourcing script: $script" >> /tmp/custom-profiled-loader.log
     source "$script"
   done
 fi
